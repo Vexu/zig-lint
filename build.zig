f528ec2bc4d3f935e9c3dd9b1623dc095cfdd6da
Vexu <git@vexu.eu>
2020-03-14 19:06:21 +0200

    initial

---
+++ build.zig
@@ -0,0 +1,20 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+
+    const tests_step = b.step("test", "Run all tests");
+    tests_step.dependOn(b.getInstallStep());
+
+    var tests = b.addTest("src/main.zig");
+    tests.setBuildMode(mode);
+    tests.dependOn(&test_step.step);
+
+    var exe = b.addExecutable("lint", "src/main.zig");
+    exe.setBuildMode(mode);
+    exe.install();
+    exe.linkLibC();
+
+    const fmt_step = b.step("fmt", "Format all source files");
+    fmt_step.dependOn(&b.addFmt(&[_][]const u8{ "build.zig", "src" }).step);
+}
